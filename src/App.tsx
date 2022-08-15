6ba33469ce76be8eeea9af04ec4475513e8cf8a7
Moisés <moisesnetored@gmail.com>
2022-08-15 17:55:59 -0400

    feat: Organizing content, moving everything to List.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import { useState } from "react";
+import { List } from "./components/List";
 
 function App() {
-  const [newItem, setNewItem] = useState('');
-  const [list, setList] = useState(['Moises', 'Geo', 'Neto', 'Besourinha', 'Gaia', 'Tobias']);
-
-  function addToList(){
-    setTimeout(() => {
-      setList(state => [...state, newItem])
-    }, 500)
-  }
-
-  function removeFromList(item: string){
-    setTimeout(() => {
-      setList(state => state.filter(item => item !== item))
-    }, 500)
-  }
-
   return (
-    <>
-      <input 
-        type="text" 
-        value={newItem} 
-        onChange={e => setNewItem(e.target.value)} 
-        placeholder="Novo item"
-      />
-
-      <button onClick={addToList}>Adicionar</button>
-      
-      <ul>
-        {list.map(item => <li key={item}>
-          {item}
-          <button onClick={()=> removeFromList(item)}>Remover</button>
-        </li>)}
-      </ul>
-    </>
+    <List initialItems={['Moises', 'Geo', 'Neto', 'Besourinha', 'Gaia', 'Tobias']} />
   )
 }
 
